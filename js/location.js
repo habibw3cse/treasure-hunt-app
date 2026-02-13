accb6b1d0171c5c63d00f0eff70de541408add9c
habibw3cse <habibw3cse@gmail.com>
2026-02-13 08:46:37 +0200

    Update location.js

--- js/location.js
+++ js/location.js
@@ -1,1 +1,15 @@
+let latitude = null;
+let longitude = null;
+
+function getLocation(){
+    navigator.geolocation.getCurrentPosition(pos=>{
+        latitude = pos.coords.latitude;
+        longitude = pos.coords.longitude;
+        console.log("Location:",latitude,longitude);
+    },()=>{
+        alert("Please enable GPS");
+    });
+}
+
+setInterval(getLocation,120000); // every 2 minutes
 
