b8fc9cbdca9dd662ff73f817c45ceed9b17b557f
habibw3cse <habibw3cse@gmail.com>
2026-02-13 08:47:08 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
+async function loadHunts(){
+    const hunts = await getHunts();
+
+    let html="";
+    hunts.forEach(h=>{
+        html += `<button onclick="startGame(${h.id})">${h.name}</button><br>`;
+    });
+
+    document.getElementById("huntList").innerHTML = html;
+}
 
